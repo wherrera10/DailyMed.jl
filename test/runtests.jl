c849f212a9958cdb0fb03cd62c51c78dcb854aa5
Bill <wherrera10@users.noreply.github.com>
2021-07-03 12:43:45 -1000

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,40 @@
 using DailyMed
 using EzXML
 using Test
+
+a, meta = applicationnumbers(extra = ["page" => "132"])
+@test last(a) == "VMF006011"
+
+a, meta = drugclasses()
+@test first(first(a).name) == '4'
+
+a, meta = drugnames(extra = ["page" => "930"])
+@test first(a)[1:11] == "ZIPRASIDONE"
+
+a, meta = ndcs(extra = ["page" => "3600"])
+@test last(a) == "W4215-S1393-01"
+
+a, meta = rxcuis(extra = ["page" => "725"])
+@test first(a).rxstring[2:3] == "21"
+
+a, meta = spls(extra = ["rxcui" => "312962"])
+first(a).title == "SIM"
+
+a, meta = spls_setid("1efe378e-fee1-4ae9-8ea5-0fe2265fe2d8")
+@test contains(a, "EDECRIN")
+
+a, meta = history("9aa7140c-012c-4ea6-866d-4732e915dab6")
+first(a).spl_version == "3"
+
+a, meta = media("1efe378e-fee1-4ae9-8ea5-0fe2265fe2d8")
+first(a).mime_type == "image/jpeg"
+
+a, meta = ndcs("1efe378e-fee1-4ae9-8ea5-0fe2265fe2d8")
+a == "42571"
+
+a, meta = packaging("l51b031a0-bc40-4159-a434-d48c3eadd2ca")
+@test contains(a, "aminolevulinic acid")
+
+a, meta = uniis(extra = ["page" => "60"])
+@test length(a) > 500
+@test a[end - 1].active_moiety == "ZUCCHINI"
